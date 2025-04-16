685d6fb9b1a0a71790e72ee22bf83e0c34cc48ed
nougzarm <nougzarm@icloud.com>
2025-04-16 08:55:58 +0200

    Suppression des warnings

--- preuve1/src/main.rs
+++ preuve1/src/main.rs
@@ -1,6 +1,6 @@
 use curve25519_dalek::ristretto::RistrettoPoint;
 use curve25519_dalek::scalar::Scalar;
-use lox_zkp::{toolbox::{prover::Prover, verifier::Verifier, SchnorrCS, TranscriptProtocol}, Transcript};
+use lox_zkp::{toolbox::TranscriptProtocol, Transcript};
 use lox_zkp::rand::Rng;
 use rand_core::OsRng; 
 
@@ -8,21 +8,21 @@ use rand_core::OsRng;
 /*  Fonction permettant de simuler une transcription honnête dans le schéma de Schnorr
     Remarque: Un tel algorithme de simulation est nécessaire pour la définition du sigma-protocole OR entre
     deux sigma-protocoles */
-fn simSC(y: &RistrettoPoint, c: &Scalar, g: &RistrettoPoint) -> (RistrettoPoint, Scalar){
+fn sim_sc(y: &RistrettoPoint, c: &Scalar, g: &RistrettoPoint) -> (RistrettoPoint, Scalar){
     let z = Scalar::random(&mut OsRng);
     let t = z*g - c*y;
     (t, z) 
 }
 
 /*  Fonction permettant de génèrer un challenge sécurisé */
-fn OR_challenge(
-    G0: &RistrettoPoint, G1: &RistrettoPoint, 
+fn or_challenge(
+    g0: &RistrettoPoint, g1: &RistrettoPoint, 
     y0: &RistrettoPoint, y1: &RistrettoPoint,
     t1: &RistrettoPoint, t2: &RistrettoPoint
 ) -> Scalar {
     let mut transcipt = Transcript::new(b"OR-Schnorr");
-    transcipt.append_point_var(b"G0", &G0);
-    transcipt.append_point_var(b"G1", &G1);
+    transcipt.append_point_var(b"G0", &g0);
+    transcipt.append_point_var(b"G1", &g1);
     transcipt.append_point_var(b"y0", &y0);
     transcipt.append_point_var(b"y1", &y1);
     transcipt.append_point_var(b"t1", &t1);
@@ -31,7 +31,7 @@ fn OR_challenge(
 }
 
 /*  XOR de deux Scalar de Ristresso */
-fn XOR_scalars(a: &Scalar, b: &Scalar) -> Scalar {
+fn xor_scalars(a: &Scalar, b: &Scalar) -> Scalar {
     let a_bytes = a.to_bytes();
     let b_bytes = b.to_bytes();
 
@@ -45,7 +45,7 @@ fn XOR_scalars(a: &Scalar, b: &Scalar) -> Scalar {
     Scalar::from_bytes_mod_order(xor_bytes)
 }
 
-
+#[allow(non_snake_case)]
 fn main() {
     /* Génération des paramètres du schéma */
     let G0 = RistrettoPoint::random(&mut OsRng);
@@ -71,20 +71,20 @@ fn main() {
         /* Simulation d'une transcription honnête pour y_d (dont on connait pas la clé privé) */
         let c_d = Scalar::random(&mut OsRng);
         let (t_d, z_d) = match d {
-            0 => simSC(&y_d, &c_d, &G0),
-            _ => simSC(&y_d, &c_d, &G1),
+            0 => sim_sc(&y_d, &c_d, &G0),
+            _ => sim_sc(&y_d, &c_d, &G1),
         };
         
         /* Génération du challenge */
         let k = Scalar::random(&mut OsRng);
         let t_b = match b {0 => k*G0, _ => k*G1};
         let c = match b {
-            0 => OR_challenge(&G0, &G1, &y_b, &y_d, &t_b, &t_d),
-            _ => OR_challenge(&G0, &G1, &y_d, &y_b, &t_d, &t_b),
+            0 => or_challenge(&G0, &G1, &y_b, &y_d, &t_b, &t_d),
+            _ => or_challenge(&G0, &G1, &y_d, &y_b, &t_d, &t_b),
         };
 
         /* Calcul de c_b */
-        let c_b = XOR_scalars(&c, &c_d);
+        let c_b = xor_scalars(&c, &c_d);
 
         /* Calcul de la réponse z_b */
         let z_b: Scalar = c_b * x + k;
@@ -107,12 +107,12 @@ fn main() {
     |----------------------------------------------------------------------------------------------------------------|
 */  
     /* Vérification des transcriptions */
-    let condition0 = (t0 == z0*G0 - c0*y0);
-    let condition1 = (t1 == z1*G1 - c1*y1);
+    let condition0  =  t0 == z0*G0 - c0*y0;
+    let condition1  =  t1 == z1*G1 - c1*y1;
 
     /* Vérification du challenge */
-    let c = OR_challenge(&G0, &G1, &y0, &y1, &t0, &t1);
-    let condition2 = (XOR_scalars(&c0, &c1) == c);
+    let c = or_challenge(&G0, &G1, &y0, &y1, &t0, &t1);
+    let condition2  =  xor_scalars(&c0, &c1) == c;
 
     /* Résultat de la vérification */
     println!("Résultat de la vérification de preuve: {}", condition0 & condition1 & condition2);
